53bd6dd15e3718cbd64c35d4cc8ad1883847a92f
mohamdmohsen <mohamedmohseen63@gmail.com>
2026-08-28 17:00:16 +0300

    initial commit

---
+++ pcrm/mydb.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+connection = psycopg2.connect(
+    dbname="mycrm",
+    host="localhost",
+    user="postgres",
+    password="12345",
+    port="5432"
+)
+
+connection.autocommit = True
+
+cursor = connection.cursor()
+
+#cursor.execute("CREATE DATABASE mycrm")
+
+cursor.close()
+connection.close()
