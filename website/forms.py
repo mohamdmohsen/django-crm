d1c019594235e4192160b2585b3bf5aaf104d3c3
mohamdmohsen <mohamedmohseen63@gmail.com>
2026-08-29 22:45:41 +0300

    finale

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
 from django import forms 
+from.models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': "form-control", "placeholder": "email address"}))
@@ -13,7 +14,7 @@ class SignUpForm(UserCreationForm):
 
 
 
-        def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
             super(SignUpForm, self).__init__(*args, **kwargs)
 
             self.fields['username'].widget.attrs['class'] = 'form-control'
@@ -30,3 +31,21 @@ class SignUpForm(UserCreationForm):
             self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
             self.fields['password2'].label = ''
             self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+
+# create add_record Form
+
+class AddRecordForm(forms.ModelForm):
+     first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="")
+     last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="")
+     email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email ", "class":"form-control"}), label="")
+     phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone ", "class":"form-control"}), label="")
+     adress = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"address", "class":"form-control"}), label="")
+     city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class":"form-control"}), label="")
+     state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class":"form-control"}), label="")
+     zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode", "class":"form-control"}), label="")
+    
+     class Meta:
+          model = Record
+          fields = '__all__'
+     
--- website/views.py
+++ website/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth import authenticate , login ,logout 
 from django.contrib import messages 
-from .forms import SignUpForm
+from .forms import SignUpForm ,AddRecordForm
+from .models import Record 
+from django.contrib.auth.decorators import login_required 
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 def home(request):
+    records = Record.objects.all 
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -19,7 +26,7 @@ def home(request):
            messages.success(request,'there is error happened')
            return redirect('home')
     else:   
-     return render( request ,'home.html',{})
+     return render( request ,'home.html',{'records': records})
 
 
 def logout_user(request):
@@ -29,8 +36,47 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == 'POST':
-          form = SignUpForm(request.POSt)
+          form = SignUpForm(request.POST)
           if form.is_valid() :
               form.save()
-              
-    return render( request ,'register.html',{})
+              messages.success(request, 'You Have Successfully Registered !')
+              return redirect('home')
+    else:
+        form = SignUpForm()      
+    return render( request ,'register.html',{'form':form})    
+
+@login_required
+def customer_record(request,pk):
+    customer_record = Record.objects.get( id=pk)
+    return render( request ,'record.html',{"customer_record":customer_record}) 
+
+@login_required
+def delete_customer(request,pk):
+    if request.method == 'POST':
+
+        delete_it = Record.objects.get(id = pk)
+        delete_it.delete()
+        messages.success(request,"Customers has been deleted")
+    return redirect("home")
+@login_required
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            add_record = form.save()
+            messages.success(request,"Customer Added Successfully!")
+            return redirect("home")
+
+    return render( request ,'add_record.html',{"form":form}) 
+@login_required
+def update_record(request,pk):
+    current_record = Record.objects.get(id=pk)
+    form = AddRecordForm(request.POST or None,instance= current_record)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Customer Updated Successfully!")
+        return redirect("home")
+    return render(request,"update_record.html",{"form": form })
+    
+
+
